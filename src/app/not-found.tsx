a5b5a41f5d70a924df69a4541653e7524f018e99
Sabrina <ssamuelrome@gmail.com>
2023-06-18 22:05:55 +0100

    Add styles and text for not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,22 @@
 import Link from "next/link";
+import { Anton, ZCOOL_QingKe_HuangYou } from "next/font/google";
+import styles from "./not-found.module.css";
+
+const zcool = ZCOOL_QingKe_HuangYou({ weight: "400", subsets: ["latin"] });
+const anton = Anton({ weight: "400", subsets: ["latin"] });
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <p>
-        Return <Link href="/">home</Link>
-      </p>
+    <div className={anton.className}>
+      <h2 className={styles.errorHeader}>Not Found</h2>
+      <div className={zcool.className}>
+        <p className={styles.notFoundText}>
+          Sorry, the page you`re trying to view does not exist
+        </p>
+        <p className={styles.notFoundText}>
+          Return <Link href="/">home</Link>
+        </p>
+      </div>
     </div>
   );
 }
